aac2d3ea4974a21a98817ae85bea85543b9651cd
조중욱 <dnr0192@highbrow-inc.com>
2014-10-27 14:23:46 +0900

    싱글톤 템플릿 추가, 유틸 함수 전용 파일 추가

---
+++ utils/Cocos2dxFunctions.cpp
@@ -0,0 +1,16 @@
+//
+//  Cocos2dxFunctions.cpp
+//
+//  Created by 조 중욱 on 2014. 10. 27..
+//
+//
+
+#include "Cocos2dxFunctions.h"
+
+USING_NS_CC;
+
+namespace ur {
+    namespace utils {
+        //cocos2d-x 유틸
+    }
+}
---
+++ utils/Cocos2dxFunctions.h
@@ -0,0 +1,19 @@
+//
+//  Cocos2dxFunctions.h
+//
+//  Created by 조 중욱 on 2014. 10. 27..
+//
+//
+
+#ifndef __Cocos2dxFunctions__
+#define __Cocos2dxFunctions__
+
+#include <cocos2d.h>
+
+namespace ur {
+    namespace utils {
+        //cocos2d-x 유틸
+    }
+}
+
+#endif
