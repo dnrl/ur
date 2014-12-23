963c5a9843ee2a51dc02f40e0a6a45ff6d58b946
조중욱 <dnr0192@highbrow-inc.com>
2014-12-23 10:06:23 +0900

    tint blink add

---
+++ action/TintBlink.cpp
@@ -0,0 +1,19 @@
+//
+//  TintBlink.cpp
+//  DragonVillage3
+//
+//  Created by 조 중욱 on 2014. 12. 19..
+//
+//
+
+#include "TintBlink.h"
+
+USING_NS_CC;
+
+TintBlink::TintBlink()
+{
+}
+
+TintBlink::~TintBlink()
+{
+}
---
+++ action/TintBlink.h
@@ -0,0 +1,28 @@
+//
+//  TintBlink.h
+//  DragonVillage3
+//
+//  Created by 조 중욱 on 2014. 12. 19..
+//
+//
+
+#ifndef __DragonVillage3__TintBlink__
+#define __DragonVillage3__TintBlink__
+
+#include <cocos2d.h>
+
+NS_CC_BEGIN
+
+class TintBlink : public cocos2d::Ref
+{
+public:
+	TintBlink(void);
+	virtual ~TintBlink(void);
+	
+protected:
+	
+};
+
+NS_CC_END
+
+#endif
